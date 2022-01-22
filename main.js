7f299b2cc4fbfe514500aa86f4794a94fe5b77a8
Thomas Emerson <emersonvisuals@gmail.com>
2022-01-22 16:36:59 +1100

    typing wip

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Digital Designer", "Frontend Developer", "UX/UI Designer"];
-const typingDelay = 200;
+const textArray = ["Designer.", "Developer.", "Creator."];
+const typingDelay = 100;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 1000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
