acb92f09ff371198b8de3024155f1596f6a7f30d
Thomas Emerson <emersonvisuals@gmail.com>
2022-02-14 11:05:57 +1100

    organised coding files

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 // Typing effect
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".cursor");
+  const textArray = ["Designer.", "Developer.", "Creator."];
+  const typingDelay = 100;
+  const erasingDelay = 100;
+  const newTextDelay = 1000; // Delay between current and next text
+  let textArrayIndex = 0;
+  let charIndex = 0;
 
-const textArray = ["Designer.", "Developer.", "Creator."];
-const typingDelay = 100;
-const erasingDelay = 100;
-const newTextDelay = 1000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    //remove 
-    cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } 
+    else {
+      //remove 
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
   }
-}
 
-function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
+  function erase() {
+    if (charIndex > 0) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if(textArrayIndex>=textArray.length) textArrayIndex=0;
+      setTimeout(type, typingDelay + 1100);
+    }
   }
-}
 
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
@@ -44,176 +43,160 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 
 
 // mobile toggle
-const trigger = document.querySelector('.mobileTrigger');
-const toggle = document.querySelector('.mobileToggle');
+  const trigger = document.querySelector('.mobileTrigger');
+  const toggle = document.querySelector('.mobileToggle');
 
-trigger.addEventListener('click', function(){
-  toggle.classList.toggle('active');
-});
+  trigger.addEventListener('click', function(){
+    toggle.classList.toggle('active');
+  });
 
 
 // desktop portfolio nav toggle
-const portfolioTrigger = document.querySelector('.navPortfolio');
-const portfolioToggle = document.querySelector('.portfolioToggle');
-const navBar = document.querySelector('nav');
+  const portfolioTrigger = document.querySelector('.navPortfolio');
+  const portfolioToggle = document.querySelector('.portfolioToggle');
+  const navBar = document.querySelector('nav');
 
 
-portfolioTrigger.addEventListener('click', function(){
-  portfolioToggle.classList.toggle('active');
-  portfolioTrigger.classList.toggle('active');
-});
+  portfolioTrigger.addEventListener('click', function(){
+    portfolioToggle.classList.toggle('active');
+    portfolioTrigger.classList.toggle('active');
+  });
 
 
 
-window.onclick = function(event) {
-  if (event.target !== portfolioTrigger) {
-    portfolioToggle.classList.remove('active');
-    portfolioTrigger.classList.remove('active');
-  }
+  window.onclick = function(event) {
+    if (event.target !== portfolioTrigger) {
+      portfolioToggle.classList.remove('active');
+      portfolioTrigger.classList.remove('active');
+    }
 }
 
 
 // line animation
+  var path = document.querySelector('#lineAnimationLine');
+  var pathTwo = document.querySelector('#lineAnimationLineTwo');
+  var length = path.getTotalLength();
+
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition = 'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();  
+
+  // Clear any previous transition
+  pathTwo.style.transition = pathTwo.style.WebkitTransition = 'none';
+  // Set up the starting positions
+  pathTwo.style.strokeDasharray = length + ' ' + length;
+  pathTwo.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  pathTwo.getBoundingClientRect();  
+
+  const portfolioContainer = document.querySelector('section.portfolio');
+
+  window.addEventListener('scroll', () => {
+    
+    // What % down is it?
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
 
-var path = document.querySelector('#lineAnimationLine');
-var pathTwo = document.querySelector('#lineAnimationLineTwo');
-var length = path.getTotalLength();
+    // Length to offset the dashes
+    var drawLength = length * scrollPercentage * 5.5;
+    var drawLengthTwo = length * scrollPercentage * 4.75;
 
-// Clear any previous transition
-path.style.transition = path.style.WebkitTransition = 'none';
-// Set up the starting positions
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-path.getBoundingClientRect();  
-
-// Clear any previous transition
-pathTwo.style.transition = pathTwo.style.WebkitTransition = 'none';
-// Set up the starting positions
-pathTwo.style.strokeDasharray = length + ' ' + length;
-pathTwo.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-pathTwo.getBoundingClientRect();  
-
-const portfolioContainer = document.querySelector('section.portfolio');
-
-window.addEventListener('scroll', () => {
-  
-  // What % down is it?
-  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    // Draw in reverse
+    path.style.strokeDashoffset = length - drawLength;
+    pathTwo.style.strokeDashoffset = length - drawLengthTwo;
 
 
-  // Length to offset the dashes
-  var drawLength = length * scrollPercentage * 5.5;
-  var drawLengthTwo = length * scrollPercentage * 4.75;
-
-  // Draw in reverse
-  path.style.strokeDashoffset = length - drawLength;
-  pathTwo.style.strokeDashoffset = length - drawLengthTwo;
-
-  // functions
-  function toggleOffPortfolio(){
-    if (drawLength > 6200) {
-      path.style.opacity = '0';
-    } else {
-      return
+    // Functions
+    function toggleOffPortfolio(){
+      if (drawLength > 6200) {
+        path.style.opacity = '0';
+      } else {
+        return
+      }
     }
-  }
-  toggleOffPortfolio();
+    toggleOffPortfolio();
 
-  function toggleOffAbout(){
-    if (drawLengthTwo > 11500) {
-      pathTwo.style.opacity = '0'
-    } else {
-      return
+    function toggleOffAbout(){
+      if (drawLengthTwo > 11500) {
+        pathTwo.style.opacity = '0'
+      } else {
+        return
+      }
     }
-  }
-  toggleOffAbout();
-
-
-
-});
+    toggleOffAbout();
+  });
 
 
 
 // parallax animation 
-window.addEventListener('scroll', function(e) {
+  window.addEventListener('scroll', function(e) {
 
-  const target = document.querySelectorAll('#scroll');
+    const target = document.querySelectorAll('#scroll');
 
 
-  var index = 0, length = target.length;
-  for(index; index < length; index++) {
-    var pos = window.pageYOffset * target[index].dataset.rate;
+    var index = 0, length = target.length;
+    for(index; index < length; index++) {
+      var pos = window.pageYOffset * target[index].dataset.rate;
 
-    if(target[index].dataset.direction === 'vertical'){
-      target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
-    
-    } else if (target[index].dataset.direction === 'horizontal') {
-      var posX = window.pageYOffset * target[index].dataset.ratex;
-      target[index].style.transform = 'translate3d('+pos+'px, 0px, 0px)';
+      if(target[index].dataset.direction === 'vertical'){
+        target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
+      
+      } else if (target[index].dataset.direction === 'horizontal') {
+        var posX = window.pageYOffset * target[index].dataset.ratex;
+        target[index].style.transform = 'translate3d('+pos+'px, 0px, 0px)';
 
-    } else if (target[index].dataset.direction === 'opacityAndScale') {
-      var posX = window.pageYOffset * target[index].dataset.ratex;
-      target[index].style.opacity = 'calc(1 - '+pos+')';
-      //target[index].style.transform = 'scale(calc(1 + '+pos+' * 0.3), calc(1 + '+pos+' * 0.3))';
-    
-    } else {
-      return
+      } else if (target[index].dataset.direction === 'opacityAndScale') {
+        var posX = window.pageYOffset * target[index].dataset.ratex;
+        target[index].style.opacity = 'calc(1 - '+pos+')';
+        //target[index].style.transform = 'scale(calc(1 + '+pos+' * 0.3), calc(1 + '+pos+' * 0.3))';
+      
+      } else {
+        return
+      }
     }
-  }
 
-});
+  });
+
+
+
 
 // burger menu toggle
-const nav = document.querySelector('nav');
+  const nav = document.querySelector('nav');
 
-trigger.addEventListener('click', function(){
-  nav.classList.toggle('active');
-});
+  trigger.addEventListener('click', function(){
+    nav.classList.toggle('active');
+  });
+
+
+
 
 // intersection observer
-const portfolio = document.querySelector('.portfolio');
-const animationLine = document.querySelectorAll('.lineAnimationContainer');
-const sections = document.querySelectorAll('section');
-const options = { 
-  root: null, // it is the viewport.
-  threshold: 0.15, // 0 to 1 scale.
-  rootMargin: "-50px"
-};
-const observer = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    if(!entry.isIntersecting) {
-      return;
-    } else {
-      observer.unobserve(entry.target);
-      entry.target.classList.toggle('active');
-    }
+  const portfolio = document.querySelector('.portfolio');
+  const animationLine = document.querySelectorAll('.lineAnimationContainer');
+  const sections = document.querySelectorAll('section');
+  const options = { 
+    root: null, // it is the viewport.
+    threshold: 0.15, // 0 to 1 scale.
+    rootMargin: "-50px"
+  };
+  const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      } else {
+        observer.unobserve(entry.target);
+        entry.target.classList.toggle('active');
+      }
+    });
+  }, options);
+
+
+  sections.forEach(section => {
+    observer.observe(section);
   });
-}, options);
-
-
-sections.forEach(section => {
-  observer.observe(section);
-});
-
-
-
-// splash screen
-/*
-const splash = document.querySelector('.splashScreen');
-const body = document.body;
-window.onload = function(e){
-  body.style.overflow = 'hidden';
-}
-
-document.addEventListener('DOMContentLoaded', function(e){
-  setTimeout(function(){
-    splash.classList.add('test');
-    body.style.overflowY = 'scroll';
-  }, 4000);
-});
-*/
